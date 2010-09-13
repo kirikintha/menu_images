29b5ed65d6ec526b9ff23a34438979abdbba47b6
Paul T. Huntsberger <Paul@paul-t-huntsbergers-macbook-pro-15.local>
2010-09-13 08:35:53 -0600

    Committing module changes and new files. This is the initial draft of the module

--- js/jquery.module.js
+++ js/jquery.module.js
@@ -7,6 +7,7 @@ if (Drupal.jsEnabled) {
   
   //jquery goes here - try not to put functions inside of here
 	$(document).ready( function() { 
+    //We have to set the file input variable, so it has a value that posts
     
   } );
   
